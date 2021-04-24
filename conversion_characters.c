796576eea2d345048c6327d8b4c58cb43bb89c94
pereiradaniel <danielpaulpereira@protonmail.com>
2021-04-23 22:19:36 -0400

    Modify

--- conversion_characters.c
+++ conversion_characters.c
@@ -1,7 +1,10 @@
+// Conversion characters determine how and where numbers print.
+
 #include <stdio.h>
 
 main ()
 {
     printf("%d roses cost %2f per %d\n", 24, 19.95, 12);
     //      24 roses cost 19.95 per 12
+    printf("%s %d %f %c\n", "Sam", 14, -8.76, 'X');
 }
