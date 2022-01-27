d2dfed7cfa54a48864606ce6456ce0c3cffe1194
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-01-27 15:46:01 -0500

    Added ex2

---
+++ CH32/ex2/ch32ex2.c
@@ -0,0 +1,25 @@
+// This program demonstrates the use of function prototypes
+
+#include <stdio.h>
+float compNet(float atomWeight, float factor);
+
+main()
+{
+    float atomWeight, factor, netWeight;
+    printf("What is the atomic weight?: ");
+    scanf(" %f", &atomWeight);
+    printf("What is the factor? ");
+    scanf(" %f", &factor);
+    netWeight = compNet(atomWeight, factor);
+    printf("The net weight is %.4f\n", netWeight);
+
+    return 0;
+}
+
+/**********************************************************************/
+
+float compNet(float atomWeight, float factor)
+{
+    float netWeight = (atomWeight - factor) * 8;
+    return(netWeight);
+}
