d06a1dbfd38de9f4c3c3a448f0f73c45a12bf359
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2024-12-12 16:46:03 -0500

    Work on poker program.

--- poker/main.c
+++ poker/main.c
@@ -96,3 +96,46 @@ main()
 }
 
 /*******************************************************/
+
+// Print a quick greeting as well as tell the users the value of
+// different winning hands
+
+void printGreeting()
+{
+
+printf("****************************************\n");
+printf("\n\n\tWelcome to the Absolute Beginner's Casino\n\n");
+printf("\tHome of Video Draw Poker\n\n");
+printf("****************************************\n");
+
+printf("Here are the rules:\n");
+printf("You start with 100 credits, and you make a bet from");
+printf("1 to 5 credits\n");
+printf("You are dealt 5 cards, and you then choose which");
+printf("cards to keep");
+printf("or discard\n");
+printf("You want to make the best possible hand.\n");
+printf("\nHere is the table for winnings (assuming a ");
+printf("bet of 1 credit):");
+printf("\nPair\t\t\t\t1 credit");
+printf("\nTwo pairs\t\t\t2 credits");
+printf("\nThree of a kind\t\t\t3 credits");
+printf("\nStraight\t\t\t4 credits");
+printf("\nFlush\t\t\t5 credits");
+printf("\nFull House\t\t\t8 credits");
+printf("\nFour of a Kind\t\t\t10 credits");
+printf("\nStraight Flush\t\t\t20 credits");
+printf("\n\nHave fun!\n\n");
+
+// Function to deal the first five cards
+
+void getFirstHand(int cardRank[], int cardSuit[]) {
+    int i, j;
+    int cardDup;
+
+    for (i=0; i<5;i++)
+    {
+        
+    }
+}
+}
